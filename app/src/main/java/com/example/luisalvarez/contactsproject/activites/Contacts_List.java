799ddc5ab9286dd8be4a05340759452334235938
Luis Alvarez <Lalvarez1223@gmail.com>
2017-05-11 18:51:17 -0500

    refactor: replaced fragments for activities
    removed the fragments because shared element transitions with no stutter worked best from activity to activity. Code for Shared Transitions works great now!

--- app/src/main/java/com/example/luisalvarez/contactsproject/activites/Contacts_List.java
+++ app/src/main/java/com/example/luisalvarez/contactsproject/activites/Contacts_List.java
@@ -1,30 +1,129 @@
 package com.example.luisalvarez.contactsproject.activites;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.luisalvarez.contactsproject.R;
-import com.example.luisalvarez.contactsproject.fragments.FragmentContactsAll;
+import com.example.luisalvarez.contactsproject.adapter.ContactListAdapter;
+import com.example.luisalvarez.contactsproject.adapter.ContactViewHolder;
+import com.example.luisalvarez.contactsproject.adapter.ListOnClickListener;
+import com.example.luisalvarez.contactsproject.data.ContactsTask;
+import com.example.luisalvarez.contactsproject.data.DataContract;
+import com.example.luisalvarez.contactsproject.data.FetchService;
+import com.example.luisalvarez.contactsproject.util.Config;
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.PeriodicTask;
 
-public class Contacts_List extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Contacts_List extends AppCompatActivity implements ListOnClickListener,LoaderManager.LoaderCallbacks {
+
+    @BindView(R.id.view_recyclerview)
+    RecyclerView vContactsList;
+    private ContactListAdapter adapter;
+    private Cursor items;
+
+    public static final String TAG_POSITION = "position";
+    public static final String TAG_URL = "url";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fragmentHolder, FragmentContactsAll.newInstance())
-                .commit();
+        ButterKnife.bind(this);
+        startFetchTaskService();
+        adapter = new ContactListAdapter(this, null,this);
+        vContactsList.setAdapter(adapter);
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
-    public void switchContent(int id, Fragment fragment) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(id, fragment, fragment.toString());
-        ft.addToBackStack(null);
-        ft.commit();
+    private void startFetchTaskService() {
+        PeriodicTask myTask = new PeriodicTask.Builder()
+                .setService(FetchService.class)
+                .setPeriod(21600L)
+                .setTag("contacts_service")
+                .build();
+        GcmNetworkManager.getInstance(this).schedule(myTask);
     }
+
+    @Override
+    public void onContactClicked(ContactViewHolder holder, int position) {
+        items.moveToPosition(position);
+        Intent intent = new Intent(Contacts_List.this, DetailActivity.class);
+        intent.putExtra(TAG_URL,items.getString(Config.POSITION_PROJECTION_CONTACT_IMAGE_SMALL));
+        intent.putExtra(TAG_POSITION,position);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            ActivityOptionsCompat options = ActivityOptionsCompat.
+                makeSceneTransitionAnimation(Contacts_List.this,
+                        holder.contactPhoto,
+                        ViewCompat.getTransitionName(holder.contactPhoto));
+            startActivity(intent, options.toBundle());
+        }else{
+            startActivity(intent);
+
+        }
+    }
+
+    /**
+     * Cursor Loading
+     */
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                DataContract.ContactsEntry.CONTENT_URI,
+                Config.buildProjectionListArray(),
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader loader, Object data) {
+       items = (Cursor) data;
+        if(items.getCount() == 0 || items == null){
+            Syncr syncr = new Syncr();
+            syncr.execute();
+        }
+        adapter.setCursor((Cursor) data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+            adapter.setCursor(null);
+    }
+
+    /**
+     * Inital loading of items
+    */
+    public class Syncr extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            //check to make sure network is up
+            ContactsTask task = new ContactsTask(getApplication());
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/luisalvarez/contactsproject/activites/DetailActivity.java
@@ -0,0 +1,49 @@
+package com.example.luisalvarez.contactsproject.activites;
+
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.example.luisalvarez.contactsproject.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private Cursor mContactData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        ImageView imageView = (ImageView) findViewById(R.id.item_contact_image);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            SharedElementTransisition(imageView);
+        }
+    }
+
+    private void SharedElementTransisition(ImageView imageView) {
+        supportPostponeEnterTransition();
+        String url = getIntent().getStringExtra(Contacts_List.TAG_URL);
+
+        Picasso.with(this)
+                .load(url)
+                .fit()
+                .noFade()
+                .centerCrop()
+                .into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        supportStartPostponedEnterTransition();
+                    }
+
+                    @Override
+                    public void onError() {
+                        supportStartPostponedEnterTransition();
+                    }
+                });
+    }
+}
